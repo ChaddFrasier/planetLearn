07b3c0b274929800bc98d76d67aa7cd7323f9a75
ChaddFrasier <33643771+ChaddFrasier@users.noreply.github.com>
2020-05-21 20:12:57 -0700

    updated testthat env

---
+++ tests/testthat/test-LinearModelL1.R
@@ -0,0 +1,7 @@
+library(testthat)
+library(planetLearn)
+
+testthat::test_that("LinearModelL1CV function checks input X.mat is of type data.frame",
+                    {
+                      expect_true(TRUE)
+                    })
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,6 +1,21 @@
 library(testthat)
+library(planetLearn)
 
 testthat::test_that("Sigmoid function returns values between 0 and 1",
                     {
-                      expect_true(TRUE)
+                      # test using vector to test many values at once
+                      result <- sigmoid(c(54.464, 12.454, 38, 1, 0, 468734, 1.233))
+                      expect_true(all(result >= 0 && result <= 1 ))
+                    })
+
+testthat::test_that("Minnaert function validates input to prevent misuse",
+                    {
+                      emission <- as.double(25.73810005)
+                      incidence <- as.double(46.18210983)
+                      k <- as.double(0.85)
+                      
+                      # incorrect use of k
+                      testthat::expect_error( minnaert( emission, incidence, as.numeric(k) ) )
+                      
+                      DN <- 0.00574214
                     })
