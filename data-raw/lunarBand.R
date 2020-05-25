1f9033947499b9d1f00f70fdbd71a8c4b5821ae0
ChaddFrasier <33643771+ChaddFrasier@users.noreply.github.com>
2020-05-25 11:55:01 -0700

    created dataset to use for package, helps w/ compression and CMD Checks

---
+++ data-raw/lunarBand.R
@@ -0,0 +1,12 @@
+library(dplyr)
+library(usethis)
+
+# set column names for the data features and label column ( DN )
+col_names <- c("DN", "Phase", "Emission", "Incidence", "LEmission", "LIncidence", "Lat", "Long", "SunAz", "CraftAz")
+
+lunarData <- readr::read_delim("data-raw/band1unit7.dat", delim = " ", col_names = col_names, trim_ws = TRUE)
+
+lunarData <- as.data.frame(lunarData)
+lunarData <- lunarData[ lunarData$Incidence <= 70 ]
+
+use_data( lunarData, overwrite = TRUE , compress = "xz")
