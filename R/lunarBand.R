6d9db960cc333fe35d4b6c08f32014946e7eb976
ChaddFrasier <33643771+ChaddFrasier@users.noreply.github.com>
2020-05-25 13:06:33 -0700

    data set documentation

---
+++ R/lunarBand.R
@@ -0,0 +1,20 @@
+#' Lunar Data from LROC WAC
+#'
+#' A dataset containing the DN, phase angle, emission angle, incidence angle, local emission angle, local incidence angle, latitude,
+#'  longitude, Sun azimuthal direction and scapecraft azimuthal direction.
+#'
+#' @format A data frame with 4,183,770 rows and 10 features:
+#' \describe{
+#'   \item{DN}{surface scattering}
+#'   \item{Phase}{The angle }
+#'   \item{Emission}{DESCRIPTION}
+#'   \item{Incidence}{DESCRIPTION}
+#'   \item{LEmission}{DESCRIPTION}
+#'   \item{LIncidence}{DESCRIPTION}
+#'   \item{Lat}{DESCRIPTION}
+#'   \item{Long}{DESCRIPTION}
+#'   \item{SunAz}{DESCRIPTION}
+#'   \item{CraftAz}{DESCRIPTION}
+#' }
+#' @source \url{http://wms.lroc.asu.edu/lroc}
+"lunarData"
