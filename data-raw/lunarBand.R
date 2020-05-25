e438624ed7c7c84cc7f6bab7b3e08e96ebbf5001
ChaddFrasier <33643771+ChaddFrasier@users.noreply.github.com>
2020-05-25 13:03:33 -0700

    data config w/ lunarData dataset

--- data-raw/lunarBand.R
+++ data-raw/lunarBand.R
@@ -4,9 +4,9 @@ library(usethis)
 # set column names for the data features and label column ( DN )
 col_names <- c("DN", "Phase", "Emission", "Incidence", "LEmission", "LIncidence", "Lat", "Long", "SunAz", "CraftAz")
 
-lunarData <- readr::read_delim("data-raw/band1unit7.dat", delim = " ", col_names = col_names, trim_ws = TRUE)
+lunarData <- readr::read_delim( "data/data-raw/band1unit7.dat", delim = " ", col_names = col_names, trim_ws = TRUE )
 
-lunarData <- as.data.frame(lunarData)
+lunarData <- as.data.frame( lunarData )
 lunarData <- lunarData[ lunarData$Incidence <= 70 ]
 
-use_data( lunarData, overwrite = TRUE , compress = "xz")
+use_data( lunarData, overwrite = TRUE, compress = "xz" )
