b87e989012eadd81e9939fd6b80b5daae95165eb
ChaddFrasier <33643771+ChaddFrasier@users.noreply.github.com>
2020-05-28 11:33:01 -0700

    better testing structure and performance

--- tests/testthat/test-LinearModelL1.R
+++ tests/testthat/test-LinearModelL1.R
@@ -1,15 +1,14 @@
 library(testthat)
 library(planetLearn)
 
+# load the data from the data folder
+load(file = "../../data/lunarData.rda")
+
+y.vec <- as.numeric(lunarData[,1])
+X.mat <- as.matrix(lunarData[,-1])
+
 testthat::test_that("LinearModelL1CV function checks input",
                     {
-                      # set the working directory to the home of the projects
-                      # load the data from the data folder
-                      load(file = "../../data/lunarData.rda")
-                      
-                      y.vec <- as.numeric(lunarData[,1])
-                      X.mat <- as.matrix(lunarData[,-1])
-                      
                       n.folds <- 10
                       set.seed(7125798)
                       fold.vec <- sample(rep(1:n.folds, l=nrow(X.mat)))
@@ -23,21 +22,22 @@ testthat::test_that("LinearModelL1CV function checks input",
 
 testthat::test_that("LinearModelL1CV function Should pass and run",
               {
-                # set the working directory to the home of the project
-                # load the data from the data folder
-                load(file = "../../data/lunarData.rda")
-                
-                y.vec <- as.numeric(lunarData[,1])
-                X.mat <- as.matrix(lunarData[,-1])
-                
                 n.folds <- 10
                 set.seed(7125798)
                 fold.vec <- sample(rep(1:n.folds, l=nrow(X.mat)))
+                
                 penalty.vec <- 10^seq(0, -2, by = -.125)
                 step.size <- .002
+                
                 result.list <- LinearModelL1CV(
                   X.mat, y.vec, fold.vec = fold.vec, n.folds = n.folds,
                   penalty.vec = penalty.vec, step.size = step.size)
                 
                 expect_true ( is.list(result.list) )
+                
+                X.test <- X.mat[which(fold.vec == 4),]
+                
+                pred.vec <- result.list$predict(X.test)
+                
+                expect_true( nrow(pred.vec) == nrow(X.test) )
               })
